140c4ba54aa736dea305b958a2cfc2d14ea6aa94
Joan Romero <romerojoan1999@gmail.com>
2023-02-08 15:24:09 -0400

    add technique H32

--- evaluate_code.py
+++ evaluate_code.py
@@ -1,12 +1,12 @@
 from bs4 import BeautifulSoup
-from techniques import t_h_25
+import techniques
 from test_result import TestResult
 
 
 def evaluate_html_code(code: str) -> list[TestResult]:
     soup = BeautifulSoup(code, "html.parser")
     results: list[TestResult] = []
-    first_results = [t_h_25(soup)]
+    first_results = [techniques.t_h_25(soup), techniques.t_h_32(soup)]
     for result in first_results:
         if result is not None:
             results.append(result)
--- techniques.py
+++ techniques.py
@@ -2,6 +2,12 @@ from bs4 import BeautifulSoup, Tag
 from test_result import TestResult
 
 
+def filter_submit_option(tag):
+    return (tag.name == "button" and tag["type"] == "submit") or (
+        tag.name == "input" and (tag["type"] == "submit" or tag["type"] == "image")
+    )
+
+
 def t_h_25(html_doc: BeautifulSoup):
     head = html_doc.find("head")
 
@@ -13,4 +19,24 @@ def t_h_25(html_doc: BeautifulSoup):
     if not title or isinstance(title, Tag) and not title.string:
         return TestResult(test_name="H25", element_count=1, error_count=1)
     else:
+        return None
+
+
+def t_h_32(html_doc: BeautifulSoup):
+    forms = html_doc.find_all("form")
+    element_count = len(forms)
+    error_count = 0
+
+    if element_count > 0:
+        for form in forms:
+            valid_form = form.find_all(filter_submit_option)
+
+            if len(valid_form) == 0:
+                error_count = error_count + 1
+
+        if error_count > 0:
+            return TestResult(
+                test_name="H32", element_count=element_count, error_count=error_count
+            )
+
         return None
